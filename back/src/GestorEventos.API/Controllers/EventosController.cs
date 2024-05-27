e6de806d580347e8f6fd451281d4cea79cda5cca
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-05-27 16:17:49 -0300

    feat: adding db

--- back/src/GestorEventos.API/Controllers/EventosController.cs
+++ back/src/GestorEventos.API/Controllers/EventosController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using GestorEventos.Application.Contracts;
 using GestorEventos.Domain;
+using GestorEventos.Domain.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -36,6 +37,43 @@ namespace GestorEventos.API.Controllers
             {                
                 return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao recuperar eventos. Erro: {ex.Message}");
             }
+        }
+
+        [HttpGet("tema/{tema}")]
+        public async Task<IActionResult> GetEventosByTema(string tema)
+        {
+          try
+          {
+            var eventosByTema = await _eventoService.GetAllEventosByTemaAsync(tema);
+            if(eventosByTema.Count == 0)
+              return NotFound("Eventos não encontrados com esse tema");
+            return Ok(eventosByTema);
+          }
+          catch (Exception ex)
+          {                
+              return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao recuperar eventos. Erro: {ex.Message}");
+          }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetEventoById(int id)
+        {
+          try
+          {
+            var evento = await _eventoService.GetEventoByIdAsync(id);
+            if(evento == null || !IsValid(evento))
+              return NotFound("Evento não encontrado");
+            return Ok(evento);
+          }
+          catch (Exception ex)
+          {                
+              return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao recuperar eventos. Erro: {ex.Message}");
+          }
+        }
+
+        private static bool IsValid(Evento evento)
+        {
+          return !evento.Tema.IsNullOrEmptyOrWhiteSpace();
         }
     }
 }
